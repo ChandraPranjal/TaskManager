3caf0c619b2323f811d115f23d08b434cfc9e29c
chandrapranjal <iampranjalchandra@gmail.com>
2024-07-14 23:48:41 +0530

    Refractored into Tasks

--- src/components/Column/Column.jsx
+++ src/components/Column/Column.jsx
@@ -1,13 +1,8 @@
 import React from 'react'
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import Item from '../Item/Item';
 
-const getItemStyle = (isDragging, draggableStyle) => ({
-    userSelect: "none",
-    padding: 2 * 2,
-    margin: `0 0 2px 0`,
-    background: isDragging ? "lightgreen" : "#e3e1e1",
-    ...draggableStyle,
-  });
+
 
 function Column({el,ind,state,setState}) {
   return (
@@ -22,43 +17,7 @@ function Column({el,ind,state,setState}) {
         {...provided.droppableProps}
       >
         {el.tasksData.map((item, index) => (
-          <Draggable
-            key={item.id}
-            draggableId={item.id}
-            index={index}
-          >
-            {(provided, snapshot) => (
-              <div
-                ref={provided.innerRef}
-                {...provided.draggableProps}
-                {...provided.dragHandleProps}
-                //    className={`p-4 select-none mb-${grid} ${snapshot.isDragging ? 'bg-lightgreen' : 'bg-gray-500'} border-gray-300`}
-                style={getItemStyle(
-                  snapshot.isDragging,
-                  provided.draggableProps.style
-                )}
-              >
-                <div
-                  style={{
-                    display: "flex",
-                    justifyContent: "space-around",
-                  }}
-                >
-                  {item.content}
-                  <button
-                    type="button"
-                    onClick={() => {
-                      const newState = [...state];
-                      newState[ind].tasksData.splice(index, 1);
-                      setState(newState);
-                    }}
-                  >
-                    delete
-                  </button>
-                </div>
-              </div>
-            )}
-          </Draggable>
+            <Item item={item} index = {index} state = {state} setState={setState} ind = {ind}/>
         ))}
         {provided.placeholder}
       </div>
--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,11 +1,50 @@
-import React from 'react'
+import React from "react";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
-function Item() {
+const getItemStyle = (isDragging, draggableStyle) => ({
+    userSelect: "none",
+    padding: 2 * 2,
+    margin: `0 0 2px 0`,
+    background: isDragging ? "lightgreen" : "#e3e1e1",
+    ...draggableStyle,
+  });
+
+function Item({ item, index, state, setState , ind }) {
   return (
-    <div>
-      
-    </div>
-  )
+    <Draggable key={item.id} draggableId={item.id} index={index}>
+      {(provided, snapshot) => (
+        <div
+          ref={provided.innerRef}
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          //    className={`p-4 select-none mb-${grid} ${snapshot.isDragging ? 'bg-lightgreen' : 'bg-gray-500'} border-gray-300`}
+          style={getItemStyle(
+            snapshot.isDragging,
+            provided.draggableProps.style
+          )}
+        >
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "space-around",
+            }}
+          >
+            {item.content}
+            <button
+              type="button"
+              onClick={() => {
+                const newState = [...state];
+                newState[ind].tasksData.splice(index, 1);
+                setState(newState);
+              }}
+            >
+              delete
+            </button>
+          </div>
+        </div>
+      )}
+    </Draggable>
+  );
 }
 
-export default Item
+export default Item;
